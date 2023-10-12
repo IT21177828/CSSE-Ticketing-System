74854a97a3073a01496364db360d015d59370569
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-10-13 03:33:18 +0530

    conn.js

---
+++ backend/db/conn.js
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+//connection to mongodb
+mongoose.connect(process.env.ATLAS_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((result) => {
+    console.log("mongo db connected!!!");
+  }).catch((err) => {
+    console.log(err);
+  });
+export default mongoose;
