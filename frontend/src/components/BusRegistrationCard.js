68984afaae8586a85ce78740b38b683781fd49e7
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-10-14 15:38:23 +0530

    Modify Bus Card

--- frontend/src/components/BusRegistrationCard.js
+++ frontend/src/components/BusRegistrationCard.js
@@ -1,40 +1,109 @@
-import React from 'react';
-
+import React, { useState } from "react";
 
 const BusRegistrationCard = (props) => {
-  // Sample bus registration data
-  console.log({props})
-  const {busName, busNumber, capacity, busRoute, rootDistance} = props;
+  const { busName, busNumber, capacity, busRoute, onEdit, onDelete } = props;
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
 
+  const openDeleteModal = () => {
+    setIsDeleteModalOpen(true);
+  };
 
+  const closeDeleteModal = () => {
+    setIsDeleteModalOpen(false);
+  };
 
   return (
-    <div className="flex justify-center min-h-fit mb-14">
-      <div className="w-full max-w-md p-4 bg-white border border-gray-300 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700">
-        <div className="text-center mb-4">
-          <h2 className="text-2xl font-bold text-gray-900 dark:text-white">Bus Details</h2>
-        </div>
-        <div className="flow-root">
-          <ul role="list" className="divide-y divide-gray-300 dark:divide-gray-700">
-            <li className="py-3 sm:py-4">
-              <div className="flex items-center space-x-4">
-                <div className="flex-1 min-w-0">
-                  <p className="text-lg font-semibold text-gray-900 dark:text-white">
-                    {busName}
-                  </p>
-                  <p className="text-sm text-gray-500 dark:text-gray-400">
-                    Bus Number: {busNumber}
-                  </p>
-                </div>
-                <div className="inline-flex items-center text-lg font-semibold text-indigo-500">
-                  Capacity: {capacity}
+    <div className="w-full max-w-xl p-6 bg-white border border-gray-300 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 mb-4 ml-2">
+      <div className="text-center mb-4">
+        <h2 className="text-2xl font-bold text-gray-900 dark:text-white">
+          {busRoute}
+        </h2>
+      </div>
+      <div className="flex flex-col space-y-2">
+        <p className="text-lg font-semibold text-gray-900 dark:text-white">
+          Bus Name: {busName}
+        </p>
+        <p className="text-sm text-gray-500 dark:text-gray-400">
+          Bus Number: {busNumber}
+        </p>
+        <p className="text-sm font-semibold text-indigo-500">
+          Capacity: {capacity}
+        </p>
+      </div>
+      <div className="flex justify-between mt-4">
+        <button
+          type="button"
+          className="text-white bg-blue-700 hover-bg-blue-800 focus-ring-4 focus-ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          onClick={onEdit}
+        >
+          Update
+        </button>
+        <button
+          type="button"
+          className="text-white bg-red-700 hover-bg-red-800 focus-ring-4 focus-ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5"
+          onClick={openDeleteModal}
+        >
+          Delete
+        </button>
+      </div>
+
+      {isDeleteModalOpen && (
+        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity">
+          <div className="fixed inset-0 flex items-center justify-center z-50">
+            <div className="relative bg-white rounded-lg shadow-xl text-left sm:max-w-lg">
+              <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    <svg
+                      className="h-6 w-6 text-red-600"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke-width="1.5"
+                      stroke="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126zM12 15.75h.007v.008H12v-.008z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <h3 className="text-base font-semibold leading-6 text-gray-900">
+                      Delete Bus
+                    </h3>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Are you sure you want to delete this bus?
+                      </p>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </li>
-            {/* Add similar list items for other fields */}
-          </ul>
+              <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                <button
+                  type="button"
+                  className="mt-3 mr-2 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover-bg-gray-50 sm:mt-0 sm:w-auto"
+                  onClick={closeDeleteModal}
+                >
+                  Cancel
+                </button>
+                <button
+                  type="button"
+                  className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover-bg-red-500 sm:w-auto"
+                  onClick={() => {
+                    closeDeleteModal();
+                    onDelete(); // Trigger delete action
+                  }}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
