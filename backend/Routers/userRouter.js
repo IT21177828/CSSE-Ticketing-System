e85aa20bd94e04baba3023ba89ed9660326cdcd1
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-10-16 22:53:41 +0530

    Updates

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -2,32 +2,17 @@ import express from "express";
 import userController from "../controllers/userController.js";
 
 const userRouter = express.Router();
+import { registerUser, checkAge } from "../controllers/userController.js";
 
-// Route to register a new user
-userRouter.post("/", userController.registerUser);
-
-// Route to log in a user
+userRouter.post("/", registerUser);
 userRouter.post("/login", userController.loginUser);
-
-// Route to check the age of a user
-userRouter.get("/checkAge", userController.checkAge);
-
-// Route to assign user roles
-userRouter.put("/userRole", userController.assignUserRole);
-
-// Route to get all users
+userRouter.get("/checkAge", checkAge);
+userRouter.put("/userRole",  userController.assignUserRole);
 userRouter.get("/all", userController.getAllUsers);
 
-// Route to refresh authentication token
 userRouter.post("/refresh", userController.verify, userController.refresh);
-
-// Route to get user details
 userRouter.post("/details", userController.verify, userController.userDetails);
-
-// Route to show the user's name
 userRouter.post("/showName", userController.verify, userController.showName);
-
-// Route to create an admin account
 userRouter.post("/adminCreate", userController.adminAccount);
 
 export default userRouter;
