ba41595e7cb2661d6921a91553d58a075939356d
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-10-16 22:51:00 +0530

    Add Comments

--- backend/controllers/conductorController.js
+++ backend/controllers/conductorController.js
@@ -1,23 +1,26 @@
+// Import Mongoose models and the database connection
 import busSchema from "../models/busModel.js";
 import mongoose from "../db/conn.js";
 import { userModel } from "./userController.js";
 import busConductorSchema from "../models/busConductor.js";
 
+// Create Mongoose models for 'bus' and 'busConductor' collections
 export const busModel = mongoose.model("bus", busSchema);
-export const busConductorModel = mongoose.model(
-  "busConductor",
-  busConductorSchema
-);
+export const busConductorModel = mongoose.model("busConductor", busConductorSchema);
 
+// Function to assign a conductor to a bus
 const assignConductor = (req, res) => {
+  // Extract busNumber and conductorId from the request body
   const { busNumber, conductorId } = req.body;
 
+  // Check if both busNumber and conductorId are provided
   if (!busNumber || !conductorId) {
     return res.send({
       message: "Please provide bus number and conductor id",
     });
   }
 
+  // Find the bus with the specified busNumber
   busModel.findOne({ busNumber: busNumber }).then((result) => {
     if (!result) {
       return res.send({
@@ -25,12 +28,15 @@ const assignConductor = (req, res) => {
       });
     }
 
+    // Update the bus to assign the conductor
     busModel
       .updateOne({ busNumber: busNumber }, { conductId: conductorId })
       .then((result) => {
-        if (result.modifiedCount == 0) {
+        if (result.modifiedCount === 0) {
           return res.send("Bus not updated");
         }
+
+        // Update the conductor's busNumber in the busConductor model
         busConductorModel
           .updateOne({ userID: conductorId }, { busNumber: busNumber })
           .then((result) => {
@@ -53,7 +59,9 @@ const assignConductor = (req, res) => {
   });
 };
 
+// Function to get all conductors
 const getAllConductors = (req, res) => {
+  // Retrieve all users with the role "conductor"
   userModel
     .find({ userRole: { $all: ["conductor"] } })
     .then((result) => {
@@ -68,7 +76,9 @@ const getAllConductors = (req, res) => {
     });
 };
 
+// Function to find conductors not assigned to a bus
 const findConductorsNotAssigned = (req, res) => {
+  // Find conductors in the busConductor model with no bus assignment
   busConductorModel
     .find({ busNumber: null })
     .then((result) => {
@@ -83,6 +93,7 @@ const findConductorsNotAssigned = (req, res) => {
     });
 };
 
+// Export all the functions as part of the module
 export default {
   assignConductor,
   getAllConductors,
